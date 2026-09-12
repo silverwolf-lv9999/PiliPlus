c6a92a4795e84b1288dd7fe89b42cf317faf0ad8
silverwolf-lv9999 <silverwolf-lv9999@users.noreply.github.com>
2026-09-12 00:47:09 +0000

    feat: 修改 applicationId 以便与现有 PiliPlus 共存

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.example.piliplus"
+        applicationId = "app.piliplus.tagfilter"
         minSdk = flutter.minSdkVersion
         targetSdk = 37
         versionCode = flutter.versionCode
